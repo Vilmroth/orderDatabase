03b1b1bc8c53453f663f7b685234ded9a5211465
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-11-13 13:58:42 +0200

    Added inster, update, delete, alter scripts

---
+++ alterData.sql
@@ -0,0 +1,7 @@
+-- ALTER TABLE table_name
+-- ADD column_name datatype; 
+-- DROP COLUMN column_name; 
+-- MODIFY COLUMN column_name datatype;
+-- ALTER COLUMN column_name datatype; 
+ALTER TABLE PRODUCT ADD COLOUR VARCHAR(100);
+ALTER TABLE DELIVERY DROP COLUMN MAXSIZE;
