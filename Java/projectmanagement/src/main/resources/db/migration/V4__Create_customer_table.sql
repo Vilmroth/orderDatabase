19d5a6fb21525f571aaaf8c5142efd463b0fb9d6
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-12-04 21:52:09 +0200

    format

--- Java/projectmanagement/src/main/resources/db/migration/V4__Create_customer_table.sql
+++ Java/projectmanagement/src/main/resources/db/migration/V4__Create_customer_table.sql
@@ -1,5 +1,5 @@
 create table customer (
-    customer int not null primary key auto_increment,
+    customer_id int not null primary key auto_increment,
     company varchar(100) not null,
     forename varchar(100) not null,
     surname varchar(100) not null,
