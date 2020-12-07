4b01565cd85c57b9ff82d42a888992e64a6ec928
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-12-07 15:17:25 +0200

    sql product, orders

---
+++ Java/projectmanagement/src/main/resources/db/migration/V7__Create_product_table.sql
@@ -0,0 +1,8 @@
+create table product ( 
+  	product_id int not null primary key auto_increment, 
+    productname varchar(100) not null,
+    price int not null, 
+    size int not null,
+    weight int not null,
+  	storage_location int,
+    foreign key (storage_location) references storages (storage_location));
