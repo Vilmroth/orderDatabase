d94c368c32d7e447dbf4b9443ec7f67053718c5c
Sneckki <75485941+Sneckki@users.noreply.github.com>
2020-12-11 15:22:06 +0200

    storages controller, dao ja manager

---
+++ Java/projectmanagement/src/main/java/dao/StoragesDAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import model.Storages;
+
+public class StoragesDAO{
+
+    public void addStorages(Storages storages){
+        try {
+            storages.save();
+        } catch (Exception e) {
+            System.out.println("Virhe: " + e.getMessage());
+        }
+    }
+}
