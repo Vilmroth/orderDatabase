bb3c8d11007e64051d287072fbd4845198600533
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-12-11 14:05:55 +0200

    added customer manager

---
+++ Java/projectmanagement/src/main/java/manager/CustomerManager.java
@@ -0,0 +1,25 @@
+package manager;
+
+import controllers.EmployeeController;
+import model.*;
+import utils.ApplicationException;
+import utils.DatabaseMigrator;
+import utils.DateTimeUtils;
+
+
+public class CustomerManager {
+
+    // private static CustomerController customercontroller = new CustomerController();
+
+    public static void main(String[] args) {
+        // System.out.println("Error: ");
+        // try {
+        //     DatabaseMigrator.doMigrations();
+        //     if (args.length > 0) {
+
+        //     }
+        // } catch (Exception e){
+        //     System.out.println("Error: " + e.getMessage());
+        // }
+    }
+}
